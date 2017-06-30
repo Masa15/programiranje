69cf5f474f5169ba253fe1acf0abef4674f92c61
Masa15 <mashamarkovickg@gmail.com>
2017-06-30 17:01:44 +0200

    Add files via upload

---
+++ osnova i sufiks.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+reč="sings"
+reč_osnova=reč [0:4]
+print (reč_osnova)
+reč_sufiks= reč [-1]
+print(reč_sufiks)
+
+reč="pevač"
+reč_osnova=reč [0:3]
+print (reč_osnova)
+reč_sufiks=reč [3:5]
+print (reč_sufiks)
+
+reč="beogradski"
+reč_osnova=reč [0:7]
+print (reč_osnova)
+reč_sufiks=reč [7:10]
+print (reč_sufiks)
+
+reč="spavanje"
+reč_osnova=reč [0:4]
+print (reč_osnova)
+reč_tematski_vokal=reč[4]
+print (reč_tematski_vokal)
+reč_sufiks=reč [5:8]
+print (reč_sufiks)
